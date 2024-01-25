4de2eaa66bce738870de7e6e91bf6b0829d778a6
Graham Clifford <gjcliff@gmail.com>
2024-01-25 02:25:43 -0600

    First iteration of moveit c++ builds, no warnings
    I followed the guide on the moveit website to do this, although they
    don't use classes just like the official ros documentation doesn't.
    Makes me wonder, should I always be using classes just like ROS code in
    python, or are C++ patterns different? I think that this will probably
    work with the launch files I wrote, but we'll see. After I write
    this file, I want to work on getting the reading from the camera more
    steady, then I'll be ready to test the naiive approach. Need to accomplish
    this by Saturday 1/27 or else I will be in trouble for the harder stuff
    down the line.

--- franka_moveit/src/franka_moveit.cpp
+++ franka_moveit/src/franka_moveit.cpp
@@ -4,42 +4,86 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "std_srvs/srv/empty.hpp"
 
+#include "geometry_msgs/msg/pose.hpp"
+
+#include "moveit/move_group_interface/move_group_interface.h"
 
 using namespace std::chrono_literals;
+using std::placeholders::_1, std::placeholders::_2;
+using moveit::planning_interface::MoveGroupInterface;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
+/* blah blah blah cstring */
 
-class MinimalPublisher : public rclcpp::Node
+class FrankaTeleop : public rclcpp::Node
 {
   public:
-    MinimalPublisher()
-    : Node("minimal_publisher"), count_(0)
+    FrankaTeleop()
+    : Node("franka_teleop", rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true)),
+    move_group_interface(MoveGroupInterface(std::make_shared<FrankaTeleop>(), "thing")),
+    count_(0)
     {
-      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+      // lambda function to create and initialize a pose msg and set it equal to target_pose
+      move_group_interface.setPoseTarget(target_pose);
+      service_ = this->create_service<std_srvs::srv::Empty>("set_and_execute_pose", std::bind(&FrankaTeleop::set_target_pose, this, _1, _2));
       timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+      500ms, std::bind(&FrankaTeleop::timer_callback, this));
     }
 
   private:
+    void set_target_pose(const std::shared_ptr<std_srvs::srv::Empty::Request> request,
+                         std::shared_ptr<std_srvs::srv::Empty::Response> response)
+    {
+      (void) request;
+      (void) response;
+      target_pose = []{
+        geometry_msgs::msg::Pose msg;
+        msg.orientation.w = 1.0;
+        msg.position.x = 0.28;
+        msg.position.y = -0.2;
+        msg.position.z = 0.5;
+        return msg;
+        }();
+        plan_target_pose();
+    }
+
+    void plan_target_pose()
+    {
+      auto const [success,plan] = [&]{
+          moveit::planning_interface::MoveGroupInterface::Plan msg;
+          auto const ok = static_cast<bool>(move_group_interface.plan(msg));
+          return std::make_pair(ok, msg);
+        }();
+
+      if (success) {
+        execute_target_pose(plan);
+      } else {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "planning failed!");
+      }
+    }
+
+    void execute_target_pose(moveit::planning_interface::MoveGroupInterface::Plan plan)
+    {
+      move_group_interface.execute(plan);
+    }
+
     void timer_callback()
     {
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      publisher_->publish(message);
+      
     }
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Service<std_srvs::srv::Empty>::SharedPtr service_;
+    // technically not what the tutorial is doing, but whatever. I can't use auto to define class members
+    moveit::planning_interface::MoveGroupInterface move_group_interface;
+    geometry_msgs::msg::Pose target_pose;
     size_t count_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<FrankaTeleop>());
   rclcpp::shutdown();
   return 0;
 }
